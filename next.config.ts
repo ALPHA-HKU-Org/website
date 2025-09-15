d9fa7b8fa8fd1b486a9adf533cc55b898a2c78c3
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2025-09-15 21:33:18 +0800

    misc(image): reduce next/images quality
    also use AI to improve white doves image quality (clean up compression artifacts) etc.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: 'export',
+  images: {
+    qualities: [50, 100],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
