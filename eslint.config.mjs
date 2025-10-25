e3fd955ac043b2132db14b17a2c879148ac9ee3e
Cheng Ho Ming, Eric <eric310@connect.hku.hk>
2025-10-26 01:55:28 +0800

    fix(eslint): correctly enable linter rules and mitigrate away from FlatCompat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,18 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 import tailwind from "eslint-plugin-tailwindcss";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      ".testing/**",
-      "build/**",
-      "next-env.d.ts",
-      "src/components/ui/**",
-    ],
-  },
-  ...compat.config({
-    extends: ["next", "next/core-web-vitals", "next/typescript", "prettier"],
-  }),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTypescript,
+  prettier,
   {
     rules: {
       "react/no-unescaped-entities": "off",
@@ -38,6 +26,15 @@ const eslintConfig = [
       },
     },
   },
-];
+  globalIgnores([
+    "node_modules/**",
+    ".next/**",
+    "out/**",
+    ".testing/**",
+    "build/**",
+    "next-env.d.ts",
+    "src/components/ui/**",
+  ]),
+]);
 
 export default eslintConfig;
